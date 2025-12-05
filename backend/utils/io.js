47a66e920221e54d8fec3bae1c7336c189624138
mingdev <alstn67@gmail.com>
2025-12-06 01:20:56 +0900

    socket 로그인 기능

--- backend/utils/io.js
+++ backend/utils/io.js
@@ -1,7 +1,19 @@
+const userController = require("../Controllers/user.controller");
+
 module.exports = function (io) {
   // io
   io.on("connection", async (socket) => {
     console.log("a user connected:", socket.id);
+
+    socket.on("login", async (userName, cb) => {
+      // 유저 정보 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("user disconnected:", socket.id);
